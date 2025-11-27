24522eff88970ed23330e2337d81631594d63b8d
DonatMiletic <94763769+DonatMiletic@users.noreply.github.com>
2025-11-27 18:15:03 +0100

    Update vjezba7.php

--- vjezba7/vjezba7.php
+++ vjezba7/vjezba7.php
@@ -1,54 +1,65 @@
 <!DOCTYPE html>
-<html lang="hr">
+
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Izračun ocjene</title>
-    <link rel="stylesheet" type="text/css" href="stil.css">
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="stil.css">
+<title>vjezba7</title>
+<style>
+</style>
 </head>
+
+
+
 <body>
-    <h1>Izračun ocjene iz kolokvija</h1>
-    <div class="container">
-        <form method="POST">
-            <label for="prvi">Ocjena I. kolokvija:</label>
-            <input type="number" id="prvi" name="prvi" min="1" max="5" required>
-            <label for="drugi">Ocjena II. kolokvija:</label>
-            <input type="number" id="drugi" name="drugi" min="1" max="5" required>
-            <input type="submit" name="gumb" value="POŠALJI">
-        </form>
-    </div>
+<h1>Izračun ocjene iz kolokvija</h1>
+ <div class="container">
+<form method="POST">
+<label for="prvi">Ocijena I kolokvija:</label><br>
+<input type="number" id="prvi"name="prvi"  min="1" max="5"><br>
+<label for="drugi">Ocijena II kolokvija:</label><br>
+<input type="number" id="drugi" name="drugi" min="1" max="5"><br>
+<input type="submit" name="gumb" value="POŠALJI">
+ </div>
+</form>
+<div class="rezultat">
+<?php 
 
-    <div class="rezultat">
-    <?php 
-    if (isset($_POST["gumb"])) {
 
-        $prvi = $_POST["prvi"];
-        $drugi = $_POST["drugi"];
-        $ocjene = array($prvi, $drugi);
-        $prosjek = ($prvi + $drugi) / 2;
 
-        if ($prvi == 1 && $drugi == 1) {
-            echo "<p>Student mora ponavljati oba kolokvija.</p>";
-            echo "<p>Konačna ocjena iz predmeta: 1</p>";
-        }
-        elseif ($prvi == 1) {
-            echo "<p>Ocjena I. kolokvija je negativna.</p>";
-            echo "<p>Konačna ocjena iz predmeta: 1</p>";
-        }
-        elseif ($drugi == 1) {
-            echo "<p>Ocjena II. kolokvija je negativna.</p>";
-            echo "<p>Konačna ocjena iz predmeta: 1</p>";
-        }
-        else {
-            echo "<p>Ocjena I. kolokvija: $prvi</p>";
-            echo "<p>Ocjena II. kolokvija: $drugi</p>";
-            echo "<hr>";
-            echo "<p>Srednja ocjena iz predmeta: $prosjek</p>";
-            echo "<p>Konačna ocjena iz predmeta: ". round($prosjek) ."</p>";
-        }
-    }
-    ?>
-    </div>
+if (isset($_POST["gumb"])) {
+    
+$prvi=$_POST["prvi"];
+$drugi=$_POST["drugi"];
+$ocjene=Array($prvi,$drugi);
+$prosjek=($prvi+$drugi)/2;
 
+if($ocjene[0]==1 && $ocjene[1]==1 ){
+
+    echo"<p>Student mora ponavljati oba kolokvija.\nOcjena I. kolokvija je 1.\nOcjena II. kolokvija je 1.</p>";
+}
+elseif($ocjene[0]==1){
+    echo"<p>Ocjena I. kolokvija je 1. Student mora ponavljati I. kolokvij</p>";
+
+}
+elseif($ocjene[1]==1){
+     echo"<p>Ocjena II. kolokvija je 1. Student mora ponavljati II. kolokvij</p>";
+}
+else{
+    echo"<p>Ocjena I. kolokvija:".$ocjene[0]."</p>";
+    echo"<p>Ocjena II. kolokvija:".$ocjene[1]."</p>";
+    echo"<hr>";
+    echo"<p>Srednja ocjena iz predmeta: ".$prosjek." </p>";
+    echo"<p>Konačna ocjena iz predmeta: ".round($prosjek)."</p>";
+}
+
+}
+?>
+ </div>
 </body>
+
+
+
 </html>
